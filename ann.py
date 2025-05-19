18306ae57ee9d9d10adab38c2fd5c1a705af31d3
Nish459 <kanishag2003@gmail.com>
2025-05-19 23:50:40 +0530

    Update in ann logic

--- ann.py
+++ ann.py
@@ -3,7 +3,7 @@ from faqs import FAQData
 import numpy as np
 import faiss
 
-# Step 1: Load model and prepare FAQ data
+# Step 1: Load pretrained model and prepare FAQ data
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 faq_data = FAQData()
@@ -11,17 +11,28 @@ faqs = faq_data.faqs
 questions = faq_data.get_questions()
 
 # Convert FAQs to embeddings (vectors)
+# 3 questions converted to 384 numbers
 faq_embeddings = model.encode(questions, convert_to_numpy=True)
 
 # Step 2: Build a FAISS Index (IVF for ANN)
-dimension = faq_embeddings.shape[1]  # 384
-n_clusters = 3  # small number for demo
+# No of columns = 384
+dimension = faq_embeddings.shape[1]  
 
-quantizer = faiss.IndexFlatL2(dimension)  # base index for clustering
+# small number for demo
+# dividing the vector spave into 3 buckets(cluster)
+n_clusters = 3  
+
+# base index for clustering
+quantizer = faiss.IndexFlatL2(dimension)
+
+# grouping similar vectors together in a cluster of 3
+# distance will be measured by L2 
 index = faiss.IndexIVFFlat(quantizer, dimension, n_clusters, faiss.METRIC_L2)
 
 # Train the index with the FAQs
-index.train(faq_embeddings)  # needed for IVF indices
+# learns data structure using k-means first
+# needed for IVF indices
+index.train(faq_embeddings)  
 index.add(faq_embeddings)
 
 # Step 3: Encode a new query
@@ -33,7 +44,7 @@ k = 1  # number of nearest neighbors to return
 distances, indices = index.search(query_embedding, k)
 
 # Step 5: Apply threshold logic
-L2_THRESHOLD = 0.75  # empirically chosen; tweak as needed
+L2_THRESHOLD = 0.75 
 matched_index = indices[0][0]
 matched_distance = distances[0][0]
 
